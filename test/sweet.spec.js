03c6e7c138aad3cfa2492dac027a2d6aa14d9773
petehunt <floydophone@gmail.com>
2014-03-18 23:38:17 -0600

    Finish test case

--- test/sweet.spec.js
+++ test/sweet.spec.js
@@ -4,28 +4,38 @@ var webpack = require('webpack');
 
 describe('sweet-loader', function() {
   it('should pretty much work', function() {
-    var BUNDLE_PATH = path.join(__dirname, 'bundle.js');
+    var done = false;
 
-    if (fs.existsSync(BUNDLE_PATH)) {
-      fs.unlinkSync(BUNDLE_PATH);
-    }
+    runs(function() {
+      var BUNDLE_PATH = path.join(__dirname, 'bundle.js');
 
-    webpack({
-      cache: true,
-      entry: path.join(__dirname, 'basic.js'),
-      output: {
-	filename: BUNDLE_PATH
-      },
-      module: {
-	loaders: [
-	  {
-	    test: /\.js$/,
-	    loader: path.join(__dirname, '../index') + '?modules[]=' + path.join(__dirname, './macros.sjs')
-	  }
-	]
+      if (fs.existsSync(BUNDLE_PATH)) {
+	fs.unlinkSync(BUNDLE_PATH);
       }
-    }, function(err, stats) {
-      console.log(err);
-    })
+
+      webpack({
+	cache: true,
+	entry: path.join(__dirname, 'basic.js'),
+	output: {
+	  filename: BUNDLE_PATH
+	},
+	module: {
+	  loaders: [
+	    {
+	      test: /\.js$/,
+	      loader: path.join(__dirname, '../index') + '?modules[]=' + path.join(__dirname, './macros.sjs')
+	    }
+	  ]
+	}
+      }, function(err, stats) {
+	expect(err).toBe(null);
+	var content = fs.readFileSync(BUNDLE_PATH, {encoding: 'utf8'});
+	expect(content.indexOf('42') > -1).toBe(true);
+	expect(content.indexOf('id(')).toBe(-1);
+	done = true;
+      });
+    });
+
+    waitsFor(function() { return done; });
   });
 });
